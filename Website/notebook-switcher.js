ff8dc0f15cf10d08989077375a7823ce38087da9
Oscar Ko <OscarKoEmail@gmail.com>
2022-10-29 12:10:35 -0400

    Add text for explaining each vis and notebook first.

--- Website/notebook-switcher.js
+++ Website/notebook-switcher.js
@@ -2,8 +2,9 @@
 const selectMenu = document.getElementById("dropdown");
 const superNotebooks = document.getElementById("supervised-container");
 const unsuperNotebooks = document.getElementById("unsupervised-container");
-const superText = document.getElementById("super-text");
-// const visTitle = document.getElementById("vis-title");
+const superText1 = document.getElementById("super-text1");
+const superText2 = document.getElementById("super-text2");
+const visTitle = document.getElementById("vis-title");
 
 selectMenu.addEventListener("change", (event) => {
 
@@ -25,20 +26,35 @@ selectMenu.addEventListener("change", (event) => {
 
 const updateTextAndTitle = (notebook) => {
 
-    const textObj = {
+    const notebookLinks = {
+        1: `<a href="https://github.com/OscarKodes/Analyzing_Relationships_with_Machine_Learning/blob/main/1%20-%20Exploratory%20Analysis%20.ipynb">EDA notebook</a>`,
+        2: `<a href="https://github.com/OscarKodes/Analyzing_Relationships_with_Machine_Learning/blob/main/1%20-%20Exploratory%20Analysis%20.ipynb">EDA notebook</a>`,
+        3: `<a href="https://github.com/OscarKodes/Analyzing_Relationships_with_Machine_Learning/blob/main/2%20-%20Classification.ipynb">classification notebook</a>`,
+        4: `<a href="https://github.com/OscarKodes/Analyzing_Relationships_with_Machine_Learning/blob/main/3%20-%20Regression.ipynb">regression notebook</a>`
+    }
+
+    const textObj1 = {
+        1: `This barchart shows the features that had the strongest correlations with each other based on the correlation matrix conducted in my ${notebookLinks[notebook]}.`,
+        2: `This barchart shows the features that had the strongest correlations with relationship quality based on the correlation matrix conducted in my ${notebookLinks[notebook]}.`,
+        3: `This barchart shows the features left in my Logistic Regression model (for predicting relationship quality) after conducting backwards elimination to remove all features that were more likely to be statistically insignifgant (where their alpha levels were over p = .05). The specific steps I took can be found in my ${notebookLinks[notebook]}.`,
+        4: `This barchart shows the features left in my Linear Regression model (for predicting relationship quality) after conducting backwards elimination to remove all features that were more likely to be statistically insignifgant (where their alpha levels were over p = .05). The specific steps I took can be found in my ${notebookLinks[notebook]}.`
+    }
+
+    const textObj2 = {
         1: "text-1",
         2: "text-2",
         3: "text-3",
         4: "text-4"
     }
 
-    // const titleObj = {
-    //     1: "Title-1",
-    //     2: "Title-2",
-    //     3: "Title-3",
-    //     4: "Title-4"
-    // }
+    const titleObj = {
+        1: "Title-1",
+        2: "Title-2",
+        3: "Title-3",
+        4: "Title-4"
+    }
 
-    superText.innerText = textObj[notebook];
-    // visTitle.innerText = titleObj[notebook];
+    superText1.innerHTML = textObj1[notebook];
+    superText2.innerText = textObj2[notebook];
+    visTitle.innerText = titleObj[notebook];
 } 
