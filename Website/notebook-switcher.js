cf4a674e3e5489325dfc24d099918123889c7384
Oscar Ko <OscarKoEmail@gmail.com>
2022-11-02 11:02:56 -0400

    Place all notebook text elements in html file. Use js file 'notebook-switcher' only to choose which text elements to show or hide.

--- Website/notebook-switcher.js
+++ Website/notebook-switcher.js
@@ -2,98 +2,76 @@
 const selectMenu = document.getElementById("dropdown");
 const superNotebooks = document.getElementById("supervised-container");
 const unsuperNotebooks = document.getElementById("unsupervised-container");
-const superText1 = document.getElementById("super-text1");
-const superText2 = document.getElementById("super-text2");
-const visTitle = document.getElementById("vis-title");
 
+const notebookLink = document.getElementById("notebook-link");
 
+const elementsFor1 = document.querySelectorAll(".n1");
+const elementsFor2 = document.querySelectorAll(".n2");
+const elementsFor3 = document.querySelectorAll(".n3");
+const elementsFor4 = document.querySelectorAll(".n4");
+
+const links = {
+    1: "https://github.com/OscarKodes/Analyzing_Relationships_with_Machine_Learning/blob/main/1%20-%20Exploratory%20Analysis%20.ipynb",
+    2: "https://github.com/OscarKodes/Analyzing_Relationships_with_Machine_Learning/blob/main/1%20-%20Exploratory%20Analysis%20.ipynb",
+    3: "https://github.com/OscarKodes/Analyzing_Relationships_with_Machine_Learning/blob/main/2%20-%20Classification.ipynb",
+    4: "https://github.com/OscarKodes/Analyzing_Relationships_with_Machine_Learning/blob/main/3%20-%20Regression.ipynb"
+}
 
 selectMenu.addEventListener("change", (event) => {
 
     // see which notebook was selected
     let selectedNotebook = event.target.value;
 
-    // hide / unhide divs based on selected notebook
+    // actions to take if the unsupervised notebook is selected
     if (selectedNotebook > 4) {
         superNotebooks.classList.add("hidden");
         unsuperNotebooks.classList.remove("hidden");
-    } else {
+    } 
+    
+    // actions to take if a supervised notebook is selected
+    else {
         superNotebooks.classList.remove("hidden");
         unsuperNotebooks.classList.add("hidden");
 
-        // update supervised text explaination
-        updateTextAndTitle(selectedNotebook);
+        // update all the text related elements
+        updateNotebookText(selectedNotebook);
+
+        // change the notebook link based on selected notebook
+        notebookLink.href = links[selectedNotebook];
     }
 
+    
 });
 
-const updateTextAndTitle = (notebook) => {
+// Hide elements for all notebooks except the first one
+const switchElements = (notebookElements, turnOn) => {
 
-    const notebookLinks = {
-        1: `<a href="https://github.com/OscarKodes/Analyzing_Relationships_with_Machine_Learning/blob/main/1%20-%20Exploratory%20Analysis%20.ipynb">EDA notebook</a>`,
-        2: `<a href="https://github.com/OscarKodes/Analyzing_Relationships_with_Machine_Learning/blob/main/1%20-%20Exploratory%20Analysis%20.ipynb">EDA notebook</a>`,
-        3: `<a href="https://github.com/OscarKodes/Analyzing_Relationships_with_Machine_Learning/blob/main/2%20-%20Classification.ipynb">classification notebook</a>`,
-        4: `<a href="https://github.com/OscarKodes/Analyzing_Relationships_with_Machine_Learning/blob/main/3%20-%20Regression.ipynb">regression notebook</a>`
+    for (let i = 0; i < notebookElements.length; i++) {
+        let e = notebookElements[i];
+
+        if (turnOn) {
+            e.classList.remove("hidden");
+        } else {
+            e.classList.add("hidden");
+        }
     }
+}
 
-    const textObj1 = {
-        1: `This barchart shows the features that had the strongest correlations with each other based on the correlation matrix conducted in my ${notebookLinks[notebook]}.`,
-        2: `This barchart shows the features that had the strongest correlations with relationship quality based on the correlation matrix conducted in the ${notebookLinks[notebook]}.`,
-        3: `This barchart shows the features left in my Logistic Regression model (for predicting relationship quality) after conducting backwards elimination to remove all features that were more likely to be statistically insignifgant (where their alpha levels were over p = .05). The specific steps I took can be found in the ${notebookLinks[notebook]}.`,
-        4: `This barchart shows the features left in my Linear Regression model (for predicting relationship quality) after conducting backwards elimination to remove all features that were more likely to be statistically insignifgant (where their alpha levels were over p = .05). The specific steps I took can be found in the ${notebookLinks[notebook]}.`
-    }
+let elementArr = [
+    elementsFor1,
+    elementsFor2,
+    elementsFor3,
+    elementsFor4
+]
 
-    const textObj2 = {
-        1: "All the correlations between the feature pairs above had a p-value of 0." +
-                "<br><br>" +
-                "<ul>" + 
-                "Notable findings:" +
-                "<li>Subjects were more likely to have partners that had similar political leanings.</li>" +
-                "<li>Subjects were more likely to have partners that had similar education levels.</li>" +
-                "<li>The older a subject was when meeting their partner, the larger their age gap tended to be.</li>" +
-                "<li>As subjects were likely to have partners that had similar education levels, their mothers were also likely to have similar education levels.</li>" +
-                "<li>The older a subject was when they met their partner, the shorter it took for them to become a couple.</li>" +
-                "</ul>",
-        2: "All the top correlations with rQual shown had p-values below 0.05." +
-                "<br><br>" +
-                "<ul>" + 
-                "Notable findings:" +
-                "<li>Income: When couples had higher household incomes, the relationship was more likely to be self-rated as 'good.'</li>" +
-                "<li>Education: When couples had higher education levels, the relationship was more likely to be self-rated as 'good.'</li>" +
-                "<li>Age: When couples were older, the relationship was more likely to be self-rated as 'good.'</li>" +
-                "<li>Children: When the subject's household had more members below the age of 18, the relationship was less likely to be self-rated as 'good.'</li>" +
-                "</ul>",
-        3: "Classification" +
-                "<br><br>" +
-                "<ul>" +
-                "Notable findings:" +
-                "<li>Income: When couples had higher household incomes, the relationship was more likely to be self-rated as 'good.'</li>" +
-                "<li>Education: When couples had higher education levels, the relationship was more likely to be self-rated as 'good.'</li>" +
-                "<li>Age: When couples were older, the relationship was more likely to be self-rated as 'good.'</li>" +
-                "<li>Sex Frequency: When the couple had sex at least once a week or more, the relationship was more likely to be self-rated as 'good.'</li>" +
-                "</ul>",
-        4: "Regression" +
-                "<br><br>" +
-                "<ul>" +
-                "Notable findings:" +
-                "<li>Income: When couples had higher household incomes, the relationship was more likely to be self-rated as 'good.'</li>" +
-                "<li>Equal Earnings: When couples earned about the same amount of income, the relationship was more likely to be self-rated as 'good.'</li>" +
-                "<li>School : When couples met in school, the relationship was more likely to be self-rated as 'good.'</li>" +
-                "<li>Work Neighbors: When couples met as work neighbors*, the relationship was more likely to be self-rated as 'good.'</li>" +
-                "<li>Age: When couples were older, the relationship was more likely to be self-rated as 'good.'</li>" +
-                "<li>Sex Frequency: When the couple had sex at least once a week or more, the relationship was more likely to be self-rated as 'good.'</li>" +
-                "</ul>" +
-                "* When consulting the codebook, it was unclear what the distinction was between coworkers and work neighbors if any.",
-    }
+const updateNotebookText = (notebook) => {
 
-    const titleObj = {
-        1: "Top Correlations between Features",
-        2: "Top Correlations between Features & Relationship Quality",
-        3: "Classification Model Feature Coefficients",
-        4: "Regression Model Feature Coefficients"
-    }
+    // turn all off
+    elementArr.map(e => switchElements(e, false));
 
-    superText1.innerHTML = textObj1[notebook];
-    superText2.innerHTML = textObj2[notebook];
-    visTitle.innerHTML = titleObj[notebook];
-} 
+    // turn selected on
+    switchElements(elementArr[notebook - 1], true);
+}
+
+// Initially show just notebook 1
+updateNotebookText(1);
