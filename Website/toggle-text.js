1efa08e0ab4178a5b43e934f7b9dc8ac0d06779a
Oscar Ko <OscarKoEmail@gmail.com>
2022-11-18 11:11:11 -0500

    Fix padding issue for collapsable by making them toggle on and off with the show-hide text

--- Website/toggle-text.js
+++ Website/toggle-text.js
@@ -2,6 +2,8 @@
 const toggleBtn = document.querySelectorAll(".toggle-btn");
 const toggleTxt = document.querySelectorAll(".toggle-txt");
 
+const collapsable = document.querySelectorAll(".collapsable");
+
 
 for (let i = 0; i < toggleBtn.length; i++) {
 
@@ -10,6 +12,7 @@ for (let i = 0; i < toggleBtn.length; i++) {
     btn.addEventListener("click", (event) => {
 
         toggleTxt[i].classList.toggle("hidden")
+        collapsable[i].classList.toggle("no-padding")
     })
 }
 
